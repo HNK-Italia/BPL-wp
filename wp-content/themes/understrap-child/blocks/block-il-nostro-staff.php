2343ebe62effd48a3c8a0a68b25a5d654e5aa932
Walter <francesco.sasso@hnk.school>
2019-06-21 20:02:38 +0200

    Primo commit sul Master

---
+++ wp-content/themes/understrap-child/blocks/block-il-nostro-staff.php
@@ -0,0 +1,13 @@
+<div class="container">
+ <hr class="spacer py-1 py-md-4" />
+ <div class="row">
+    <img class="col-md-7" src="staff.png" alt="Immagine Il nostro staff">
+    <div class="col-md-5">
+     <div class="ventipx">
+        <h2 class="title"><?php block_field('titolo'); ?></h2>
+        <p><?php block_field('testo-in-piccolo'); ?></p>
+     </div>
+    </div>
+ </div>
+ <hr class="spacer py-1 py-md-4" />
+</div>
