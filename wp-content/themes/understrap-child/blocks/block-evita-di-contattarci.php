150a6346ff2c961cbd52de3ba6d04b719f3d0351
Walter <francesco.sasso@hnk.school>
2019-06-22 13:38:18 +0200

    Aggiunge i primi 2 blocchi

---
+++ wp-content/themes/understrap-child/blocks/block-evita-di-contattarci.php
@@ -0,0 +1,26 @@
+<section class="container-fluid backgroundgradient">
+    <section class="container text-center">
+      <hr class="spacer py-1 py-md-4" />
+        <h2><?php block_field('evita-titolo'); ?></h2>
+        <div class="row">
+          <div class="col-md-4">
+            <i class="material-icons" style="color:#ffffff">close</i>
+            <h4 style="padding-bottom: 25px"><?php block_field('cerchi-un-diplomificio'); ?></h4>
+            <h6><?php block_field('cerchi-un-diplomificio-testo-1'); ?></h6>
+            <h6><?php block_field('cerchi-un-diplomificio-testo-2'); ?></h6>
+          </div>
+          <div class="col-md-4">
+            <img src="stop.svg" height="100%" width="50%" />
+          </div>
+          <div class="col-md-4">
+            <i class="material-icons" style="color:#ffffff">close</i>
+            <h4 style="padding-bottom: 25px"><?php block_field('cerchi-lavoro'); ?></h4>
+            <h6><?php block_field('cerchi-lavoro-testo-1'); ?></h6>
+            <h6><?php block_field('cerchi-lavoro-testo-2'); ?></h6>
+            <h6><?php block_field('cerchi-lavoro-testo-3'); ?></h6>
+            <h6><?php block_field('cerchi-lavoro-testo-4'); ?></h6>
+          </div>
+        </div>
+      <hr class="spacer py-1 py-md-4" />
+    </section>
+  </section>
