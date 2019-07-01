e3b99d48d5de8e84c74b78c38c8b10383d570880
Walter <francesco.sasso@hnk.school>
2019-07-01 19:05:49 +0200

    Mette aposto la sezione Sei un genitore

--- wp-content/themes/understrap-child/blocks/block-evita-di-contattarci.php
+++ wp-content/themes/understrap-child/blocks/block-evita-di-contattarci.php
@@ -1,18 +1,17 @@
-<section class="container-fluid backgroundgradient">
+<section class="row backgroundgradient" style="position: relative">
+<img class="icona-evita-divieto" src="<?php echo get_stylesheet_directory_uri()?>/img/divieto.svg" style="position: absolute; right: 20%; top: 45%; max-width: 128px; height: auto; color: rgba(255,255,255,0.2);">
+<img class="icona-evita-esclamativo" src="<?php echo get_stylesheet_directory_uri()?>/img/punto esclamativo.svg" style="position: absolute; left: 20%; top: 15%; max-width: 128px; height: auto;  transform: rotate(5deg);">
     <section class="container text-center">
       <hr class="spacer py-1 py-md-4" />
         <h2><?php block_field('evita-titolo'); ?></h2>
         <div class="row">
-          <div class="col-md-4">
+          <div class="col-md-6">
             <i class="material-icons" style="color:#ffffff">close</i>
             <h4 style="padding-bottom: 25px"><?php block_field('cerchi-un-diplomificio'); ?></h4>
             <h6><?php block_field('cerchi-un-diplomificio-testo-1'); ?></h6>
             <h6><?php block_field('cerchi-un-diplomificio-testo-2'); ?></h6>
           </div>
-          <div class="col-md-4">
-            <img src="<?php block_field('immagine-centrale'); ?>" height="100%" width="50%" />
-          </div>
-          <div class="col-md-4">
+          <div class="col-md-6">
             <i class="material-icons" style="color:#ffffff">close</i>
             <h4 style="padding-bottom: 25px"><?php block_field('cerchi-lavoro'); ?></h4>
             <h6><?php block_field('cerchi-lavoro-testo-1'); ?></h6>
--- wp-content/themes/understrap-child/header.php
+++ wp-content/themes/understrap-child/header.php
@@ -19,6 +19,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons|Open+Sans:300,400,700,800" rel="stylesheet">
 	<?php wp_head(); ?>
 </head>
 
