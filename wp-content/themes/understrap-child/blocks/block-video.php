ec8b39214ca9dce554e81ccaf57e6c2b8b2906a4
Walter <francesco.sasso@hnk.school>
2019-07-15 18:47:56 +0200

    Aggiunge 2 modi per il video in Home

--- wp-content/themes/understrap-child/blocks/block-video.php
+++ wp-content/themes/understrap-child/blocks/block-video.php
@@ -8,3 +8,14 @@
   </div>
 </div>
 </section>
+
+ <!-- Inizio zona TEST -->
+
+<section class="container" style="position: relative">
+  <div style="color: <?php block_field('colore'); ?>; position: relative; height: 100%;">
+    <h1 class="text-center" style="position: absolute; left: 50%; top: 60%; transform:translate(-50%);"><?php block_field('titolo'); ?></h1>
+    <h5 class="text-center" style="position: absolute; left: 50%; top: 80%; transform:translate(-50%);"><?php block_field('testo'); ?></h5>
+    <a href="<?php block_field('link'); ?>"><img class="icona-genitore" src="<?php echo get_stylesheet_directory_uri()?>/img/play.svg" style="position: absolute; left: 50%; top: 30%; transform:translate(-50%); max-width: 128px; height: auto;"></a>
+    <img style="z-index: -1;" src="<?php block_field('immagine'); ?>">
+ </div>
+</section>
