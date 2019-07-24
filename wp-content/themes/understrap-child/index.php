763922219ab5c46ba163a4656812e4d2b499222d
Walter <francesco.sasso@hnk.school>
2019-07-24 14:30:02 +0200

    Sistema il single e index .php nel understrap-child

--- wp-content/themes/understrap-child/index.php
+++ wp-content/themes/understrap-child/index.php
@@ -10,14 +10,18 @@
  *
  * @package understrap
  */
-// Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
 get_header();
+
 $container = get_theme_mod( 'understrap_container_type' );
 ?>
 
 <?php if ( is_front_page() && is_home() ) : ?>
-	<?php get_template_part( 'global-templates/hero' ); ?>
+<?php get_template_part( 'global-templates/hero' ); ?>
 <?php endif; ?>
 
 <div class="wrapper" id="index-wrapper">
@@ -30,30 +34,42 @@ $container = get_theme_mod( 'understrap_container_type' );
 			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
 
 			<main class="site-main" id="main">
+		  <div class="row">
+			<div class="hero-minimo" style="background-image: url('');">
+					<div class="trasparenza">
+						<div class="">
+							<h1 class="text-center" style="position: absolute; left: 50%; top: 60%; transform:translate(-50%);">
+							<?php echo get_theme_mod('showcase_citta_legale', 'Blog'); ?></h1>
+						</div>
+					</div>
+				</div>
+		  </div>
 
 				<?php if ( have_posts() ) : ?>
-
+				<div class="container">
 					<?php /* Start the Loop */ ?>
 
 					<?php while ( have_posts() ) : the_post(); ?>
 
-						<?php
+					<?php
+
 						/*
 						 * Include the Post-Format-specific template for the content.
 						 * If you want to override this in a child theme, then include a file
 						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 						 */
+						
 						get_template_part( 'loop-templates/content', get_post_format() );
 						?>
 
 					<?php endwhile; ?>
 
-				<?php else : ?>
+					<?php else : ?>
 
 					<?php get_template_part( 'loop-templates/content', 'none' ); ?>
 
-				<?php endif; ?>
-
+					<?php endif; ?>
+				</div><!-- ./ end container -->
 			</main><!-- #main -->
 
 			<!-- The pagination component -->
@@ -68,4 +84,4 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 </div><!-- #index-wrapper -->
 
-<?php get_footer();
+<?php get_footer(); ?>
--- wp-content/themes/understrap-child/single.php
+++ wp-content/themes/understrap-child/single.php
@@ -4,30 +4,44 @@
  *
  * @package understrap
  */
-// Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
 get_header();
 $container = get_theme_mod( 'understrap_container_type' );
 ?>
 
-<div class="wrapper" id="single-wrapper">
+<div class="wrapper container" id="single-wrapper">
 
 	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
 
 		<div class="row">
 
 			<!-- Do the left sidebar check -->
-			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
+
+			<div class="hero-minimo" style="background-image: url('<?php the_post_thumbnail_url(); ?>'); background-position: center center;">
+				<div class="trasparenza">
+					<h1 class="text-center" style="position: absolute; left: 50%; top: 60%; transform:translate(-50%);">
+						<?php single_post_title(); ?></h1>
+				</div>
+			</div>
 
 			<main class="site-main" id="main">
 
+				<?php the_content(); ?>
+
+				<hr class="spacer py-1 py-md-4">
+
+				<!-- Ciao belli Autore -->
+
+				<?php // echo the_author_meta('user_level') ?>
+
+				<!-- Ciao belli Autore -->
+
 				<?php while ( have_posts() ) : the_post(); ?>
-
-					<?php get_template_part( 'loop-templates/content', 'single' ); ?>
-
-					<?php understrap_post_nav(); ?>
-
-					<?php
+				<?php
 					// If comments are open or we have at least one comment, load up the comment template.
 					if ( comments_open() || get_comments_number() ) :
 						comments_template();
@@ -47,4 +61,4 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 </div><!-- #single-wrapper -->
 
-<?php get_footer();
+<?php get_footer(); ?>
