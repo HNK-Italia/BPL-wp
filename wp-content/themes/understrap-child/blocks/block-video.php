c72ee259a39aca1c5f6939a4a0476e879dae7dbf
Walter <francesco.sasso@hnk.school>
2019-07-27 20:18:53 +0200

    Sistema come da progetto il video iniziale

--- wp-content/themes/understrap-child/blocks/block-video.php
+++ wp-content/themes/understrap-child/blocks/block-video.php
@@ -1,10 +1,9 @@
 <section class="row">
-  <div class="container-fluid" style="position: relative; padding: 0; background: #000000">
-    <div style="color: <?php block_field('colore'); ?>; position: relative; height: 525px;">
-      <a href="https://www.youtube.com/watch?v=<?php block_field('link'); ?>" rel="noopener noreferrer" target="_blank"><img class="icona-play" src="<?php echo get_stylesheet_directory_uri()?>/img/play.svg"></a>
-      <h1 class="text-center" style="position: absolute; left: 50%; bottom: 5%; transform:translate(-50%);"><?php block_field('titolo'); ?></h1>
-      <h5 class="text-center" style="position: absolute; left: 50%; bottom: 5%; transform:translate(-50%);"><?php block_field('testo'); ?></h5>
+  <div class="container-fluid" style="position: relative; padding: 0; background-image: url('<?php block_field('immagine'); ?>'); background-size: cover; background-position: center top;">
+    <div style="color: <?php block_field('colore'); ?>; position: relative; height: 550px;">
+      <a href="<?php block_field('link'); ?>" rel="noopener noreferrer" target="_blank"><img class="icona-play" src="<?php echo get_stylesheet_directory_uri()?>/img/play.svg"></a>
+      <h1 class="text-center" style="position: absolute; left: 50%; bottom: 17%; transform:translate(-50%);"><?php block_field('titolo'); ?></h1>
+      <h5 class="text-center" style="position: absolute; left: 50%; bottom: 2%; transform:translate(-50%);"><?php block_field('testo'); ?></h5>
     </div>
-  <hr class="spacer py-4 py-md-4">
   </div>
 </section>
