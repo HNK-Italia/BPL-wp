8eb4142cfd6f075559c14e53a083066f54bebf2e
Walter <francesco.sasso@hnk.school>
2019-07-27 16:55:52 +0200

    Rende il video da iframe a tasto con il collegamento

--- wp-content/themes/understrap-child/blocks/block-video.php
+++ wp-content/themes/understrap-child/blocks/block-video.php
@@ -1,28 +1,10 @@
 <section class="row">
-<div class="container-fluid" style="position: relative; padding: 0;">
-  <div style="color: <?php block_field('colore'); ?>; position: relative; height: 525px;">
-   <div class="visibile">
-    <h1 class="text-center" style="position: absolute; left: 50%; bottom: 17%; transform:translate(-50%);"><?php block_field('titolo'); ?></h1>
-    <h5 class="text-center" style="position: absolute; left: 50%; bottom: 5%; transform:translate(-50%);"><?php block_field('testo'); ?></h5>
-   </div>
-    <iframe width="100%" height="550px" src="https://www.youtube.com/embed/<?php block_field('link'); ?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <div class="container-fluid" style="position: relative; padding: 0; background: #000000">
+    <div style="color: <?php block_field('colore'); ?>; position: relative; height: 525px;">
+      <a href="https://www.youtube.com/watch?v=<?php block_field('link'); ?>" rel="noopener" target="_blank"><img class="icona-play" src="<?php echo get_stylesheet_directory_uri()?>/img/play.svg"></a>
+      <h1 class="text-center" style="position: absolute; left: 50%; bottom: 5%; transform:translate(-50%);"><?php block_field('titolo'); ?></h1>
+      <h5 class="text-center" style="position: absolute; left: 50%; bottom: 5%; transform:translate(-50%);"><?php block_field('testo'); ?></h5>
+    </div>
+  <hr class="spacer py-4 py-md-4">
   </div>
-<hr class="spacer py-4 py-md-4">
-</div>
 </section>
-
- <!-- Inizio zona TEST -->
-
-<script type="text/javascript">
-
-  var container = document.querySelector('#testone');
-
-    container.addEventListener('click', function(){
-        this.classList.remove('visibile');
-        this.classList.add('nascosto');
-  })
-    container.addEventListener('click', function(){
-        this.classList.add('visibile');
-        this.classList.remove('nascosto');
-  })
-</script>
