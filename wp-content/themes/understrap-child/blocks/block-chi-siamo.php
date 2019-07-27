530f7e86f6065bd31277541d505ce70a0c777f64
Walter <francesco.sasso@hnk.school>
2019-07-27 12:40:56 +0200

    Aggiunge i link nel blocco insegnanti

--- wp-content/themes/understrap-child/blocks/block-chi-siamo.php
+++ wp-content/themes/understrap-child/blocks/block-chi-siamo.php
@@ -1,10 +1,10 @@
 <section class="row" style="background-color: #1CA554;">
   <section class="container">
     <hr class="spacer py-1 py-md-4" />
-      <div class="text-center">
+      <div class="">
         <h2 style="color: #FFFFFF;"><?php block_field('titolo'); ?></h2>
       </div>
-    <div class="text-center" style="color: #FFFFFF;">
+    <div class="" style="color: #FFFFFF;">
       <p><?php block_field('testo1'); ?></p>
       <p><?php block_field('testo2'); ?></p>
       <p><?php block_field('testo3'); ?></p>
--- wp-content/themes/understrap-child/blocks/block-insegnanti.php
+++ wp-content/themes/understrap-child/blocks/block-insegnanti.php
@@ -9,33 +9,33 @@
   <div class="row">
 
     <div class="col-mx-12 col-md-6 col-lg-3">
-      <img src="<?php block_field('foto1'); ?>" style="width: 200px; height: 200px; border-radius: 50%; margin-bottom: 20px;" />
+      <a href="<?php block_field('link1'); ?>"><img src="<?php block_field('foto1'); ?>" style="width: 200px; height: 200px; border-radius: 50%; margin-bottom: 20px;" />
       <h5 style="color: #1CA554; margin-botom: 15px;"><?php block_field('prof1'); ?></h4>
-      <h6><?php block_field('materia1'); ?></h6>
+      <h6><?php block_field('materia1'); ?></h6></a>
     </div>
 
   <!-- Inizio di una card e fine di un'altra -->
 
     <div class="col-mx-12 col-md-6 col-lg-3">
-      <img src="<?php block_field('foto2'); ?>" style="width: 200px; height: 200px; border-radius: 50%; margin-bottom: 20px;" />
+      <a href="<?php block_field('link2'); ?>"><img src="<?php block_field('foto2'); ?>" style="width: 200px; height: 200px; border-radius: 50%; margin-bottom: 20px;" />
       <h5 style="color: #1CA554; margin-bottom: 15px;"><?php block_field('prof2'); ?></h4>
-      <h6><?php block_field('materia2'); ?></h6>
+      <h6><?php block_field('materia2'); ?></h6></a>
     </div>
 
   <!-- Inizio di una card e fine di un'altra -->
 
     <div class="col-mx-12 col-md-6 col-lg-3">
-      <img src="<?php block_field('foto3'); ?>" style="width: 200px; height: 200px; border-radius: 50%; margin-bottom: 20px;" />
+      <a href="<?php block_field('link3'); ?>"><img src="<?php block_field('foto3'); ?>" style="width: 200px; height: 200px; border-radius: 50%; margin-bottom: 20px;" />
       <h5 style="color: #1CA554; margin-botom: 15px;"><?php block_field('prof3'); ?></h4>
-      <h6><?php block_field('materia3'); ?></h6>
+      <h6><?php block_field('materia3'); ?></h6></a>
     </div>
 
 <!-- Inizio di una card e fine di un'altra -->
 
     <div class="col-mx-12 col-md-6 col-lg-3">
-      <img src="<?php block_field('foto4'); ?>" style="width: 200px; height: 200px; border-radius: 50%; margin-bottom: 20px;" />
+      <a href="<?php block_field('link4'); ?>"><img src="<?php block_field('foto4'); ?>" style="width: 200px; height: 200px; border-radius: 50%; margin-bottom: 20px;" />
       <h5 style="color: #1CA554; margin-botom: 15px;"><?php block_field('prof4'); ?></h4>
-      <h6><?php block_field('materia4'); ?></h6>
+      <h6><?php block_field('materia4'); ?></h6></a>
     </div>
 
 
