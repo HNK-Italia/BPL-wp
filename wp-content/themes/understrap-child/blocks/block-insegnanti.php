7d4f007da2566f6a8632e6a123b0cf0b4b6c8ff3
Walter <francesco.sasso@hnk.school>
2019-07-28 08:08:23 +0200

    Aggiunge le animazioni e le ombre agli insegnanti

--- wp-content/themes/understrap-child/blocks/block-insegnanti.php
+++ wp-content/themes/understrap-child/blocks/block-insegnanti.php
@@ -1,39 +1,39 @@
-<section class="container mb-5 text-center">
+<section class="container text-center">
 
-  <div style="padding: 20px;">
+  <div class="titoloinsegnanti">
     <h2 style="color: #1CA554;"><?php block_field('titolo'); ?></h2>
   </div>
 
   <!-- Inizio delle cards Insegnanti -->
 
-  <div class="row">
+  <div class="row boxinsegnanti">
 
-    <div class="col-mx-12 col-md-6 col-lg-3">
-      <a href="<?php block_field('link1'); ?>"><img src="<?php block_field('foto1'); ?>" style="width: 200px; height: 200px; border-radius: 50%; margin-bottom: 20px;" />
+    <div class="col-mx-12 col-md-6 col-lg-3 tastoinsegnanti">
+      <a class="tastoinsegnanti" href="<?php block_field('link1'); ?>"><img src="<?php block_field('foto1'); ?>" class="immagineinsegnanti" />
       <h5 style="color: #1CA554; margin-botom: 15px;"><?php block_field('prof1'); ?></h4>
       <h6><?php block_field('materia1'); ?></h6></a>
     </div>
 
   <!-- Inizio di una card e fine di un'altra -->
 
-    <div class="col-mx-12 col-md-6 col-lg-3">
-      <a href="<?php block_field('link2'); ?>"><img src="<?php block_field('foto2'); ?>" style="width: 200px; height: 200px; border-radius: 50%; margin-bottom: 20px;" />
+    <div class="col-mx-12 col-md-6 col-lg-3 tastoinsegnanti">
+      <a class="tastoinsegnanti" href="<?php block_field('link2'); ?>"><img src="<?php block_field('foto2'); ?>" class="immagineinsegnanti" />
       <h5 style="color: #1CA554; margin-bottom: 15px;"><?php block_field('prof2'); ?></h4>
       <h6><?php block_field('materia2'); ?></h6></a>
     </div>
 
   <!-- Inizio di una card e fine di un'altra -->
 
-    <div class="col-mx-12 col-md-6 col-lg-3">
-      <a href="<?php block_field('link3'); ?>"><img src="<?php block_field('foto3'); ?>" style="width: 200px; height: 200px; border-radius: 50%; margin-bottom: 20px;" />
+    <div class="col-mx-12 col-md-6 col-lg-3 tastoinsegnanti">
+      <a class="tastoinsegnanti" href="<?php block_field('link3'); ?>"><img src="<?php block_field('foto3'); ?>" class="immagineinsegnanti" />
       <h5 style="color: #1CA554; margin-botom: 15px;"><?php block_field('prof3'); ?></h4>
       <h6><?php block_field('materia3'); ?></h6></a>
     </div>
 
 <!-- Inizio di una card e fine di un'altra -->
 
-    <div class="col-mx-12 col-md-6 col-lg-3">
-      <a href="<?php block_field('link4'); ?>"><img src="<?php block_field('foto4'); ?>" style="width: 200px; height: 200px; border-radius: 50%; margin-bottom: 20px;" />
+    <div class="col-mx-12 col-md-6 col-lg-3 tastoinsegnanti">
+      <a class="tastoinsegnanti" href="<?php block_field('link4'); ?>"><img src="<?php block_field('foto4'); ?>" class="immagineinsegnanti" />
       <h5 style="color: #1CA554; margin-botom: 15px;"><?php block_field('prof4'); ?></h4>
       <h6><?php block_field('materia4'); ?></h6></a>
     </div>
