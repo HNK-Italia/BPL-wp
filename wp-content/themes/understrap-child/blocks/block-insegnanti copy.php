6f08d62510e486017144d8a41d48df06f9512cd1
Walter <francesco.sasso@hnk.school>
2019-07-28 08:30:28 +0200

    Aggiunge il file bloack-insegnanti copy.php ma non funziona l'if

--- wp-content/themes/understrap-child/blocks/block-insegnanti copy.php
+++ wp-content/themes/understrap-child/blocks/block-insegnanti copy.php
@@ -8,13 +8,29 @@
 
   <div class="row boxinsegnanti">
 
+  <!-- Parte dell IF -->
+
+  <?php if (block_field('foto1')) { ?>
+
+  <!-- Parte dell IF -->
+
     <div class="col-mx-12 col-md-6 col-lg-3 tastoinsegnanti">
       <a href="<?php block_field('link1'); ?>"><img src="<?php block_field('foto1'); ?>" class="immagineinsegnanti" />
       <h5 style="color: #1CA554; margin-botom: 15px;"><?php block_field('prof1'); ?></h4>
       <h6><?php block_field('materia1'); ?></h6></a>
     </div>
 
+<?php
+	}
+?>
+
   <!-- Inizio di una card e fine di un'altra -->
+
+  <!-- Parte dell IF -->
+
+  <?php if (block_field('foto2')) { ?>
+
+  <!-- Parte dell IF -->
 
     <div class="col-mx-12 col-md-6 col-lg-3 tastoinsegnanti">
       <a href="<?php block_field('link2'); ?>"><img src="<?php block_field('foto2'); ?>" class="immagineinsegnanti" />
@@ -22,7 +38,17 @@
       <h6><?php block_field('materia2'); ?></h6></a>
     </div>
 
+<?php
+	}
+?>
+
   <!-- Inizio di una card e fine di un'altra -->
+
+  <!-- Parte dell IF -->
+
+  <?php if (block_field('foto3')) { ?>
+
+  <!-- Parte dell IF -->
 
     <div class="col-mx-12 col-md-6 col-lg-3 tastoinsegnanti">
       <a href="<?php block_field('link3'); ?>"><img src="<?php block_field('foto3'); ?>" class="immagineinsegnanti" />
@@ -30,7 +56,17 @@
       <h6><?php block_field('materia3'); ?></h6></a>
     </div>
 
+<?php
+	}
+?>
+
 <!-- Inizio di una card e fine di un'altra -->
+
+  <!-- Parte dell IF -->
+
+  <?php if (block_field('foto4')) { ?>
+
+  <!-- Parte dell IF -->
 
     <div class="col-mx-12 col-md-6 col-lg-3 tastoinsegnanti">
       <a href="<?php block_field('link4'); ?>"><img src="<?php block_field('foto4'); ?>" class="immagineinsegnanti" />
@@ -38,6 +74,10 @@
       <h6><?php block_field('materia4'); ?></h6></a>
     </div>
 
+
+<?php
+	}
+?>
 
 <!-- Fine delle cards Insegnanti -->
 
